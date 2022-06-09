8c8df555694ac587900a6aaee7461c6c0508364b
Ludo-Dpe <ludovic.delplanque@epitech.eu>
2022-06-09 17:36:04 +0200

    up

---
+++ My project/Assets/Scripts/Scene management/Portal.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+public class Portal : MonoBehaviour, IPlayerTrigerrable
+{
+    private void OnTriggerEnter2D(PlayerController player)
+    {
+
+    }
+}
+*/
