a721b92e2c59f03b238429d44b1aaa093541c094
DoctypeHonoo <guillaume.lemoine@epitech.eu>
2022-06-10 15:56:34 +0200

    push pause menu

--- My project/Assets/Scripts/GameController.cs
+++ My project/Assets/Scripts/GameController.cs
@@ -2,16 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public enum GameState { FreeRoam, Dialog }
 public class GameController : MonoBehaviour
 {
     public Text keyCounter;
 
+    public Canvas Menu_Canvas;
+
+    public static bool isPaused;
+
     [SerializeField] PlayerController playerController;
     GameState state;
     private void Start()
     {
+        Menu_Canvas.gameObject.SetActive(false);
+
         DialogManager.Instance.OnShowDialog += () =>
         {
             state = GameState.Dialog;
@@ -25,6 +32,14 @@ public class GameController : MonoBehaviour
 
     private void Update()
     {
+        if (!Menu_Canvas.gameObject.activeSelf && isPaused) {
+            isPaused = !isPaused;
+            PauseGame();
+        }
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            isPaused = !isPaused;
+            PauseGame();
+        }
         if (state == GameState.FreeRoam)
         {
            playerController.HandleUpdate();
@@ -34,5 +49,19 @@ public class GameController : MonoBehaviour
             DialogManager.Instance.HandleUpdate();
         }
         keyCounter.text = "Keys : " + Globals.keyCount;
+    }
+
+    public void PauseGame()
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 0f;
+            Menu_Canvas.gameObject.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            Menu_Canvas.gameObject.SetActive(false);
+        }
     }
 }
