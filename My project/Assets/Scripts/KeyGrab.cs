82873bc0745817c76fab13210ad95662d4fd8751
Ludo-Dpe <ludovic.delplanque@epitech.eu>
2022-06-11 08:42:43 +0200

    Up presque fin

--- My project/Assets/Scripts/KeyGrab.cs
+++ My project/Assets/Scripts/KeyGrab.cs
@@ -7,14 +7,26 @@ public class KeyGrab : MonoBehaviour
     public GameObject GameManager;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player") {
+        if (collision.tag == "Player")
+        {
             Destroy(gameObject);
             Globals.keyCount++;
+            if (Globals.keyCount == 2)
+            {
+                Debug.Log("Test QUit");
+                Application.Quit();
+            }
         }
 
-        /*if (Globals.keyCount == 2)
-        {
-            GameManager.GetComponent<GameController>().EndGame();
-        }*/
     }
+
+    /*void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Finish")
+        {
+            Debug.Log("finish");
+            Application.Quit();
+        }
+    }
+}*/
 }
