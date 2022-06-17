a4a283afd35eb91efface639d20a8e89d58dc463
DoctypeHonoo <guillaume.lemoine@epitech.eu>
2022-06-17 10:29:19 +0200

    yooo les gars

--- My project/Assets/Globals.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Globals : MonoBehaviour
-{
-    public static int stoneCount = 0;
-    public static int countTime = 10;
-}
